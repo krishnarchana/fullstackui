77fb6bf618c3fe30325fc64619da5eaa1aa9488a
gopikrishna <gopikrishna.e84@gmail.com>
2017-11-17 11:44:57 +0530

    angular and db changes

--- database/test.sql
+++ database/test.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.20, for Win64 (x86_64)
 --
--- Host: localhost    Database: Krishna!8984
+-- Host: localhost    Database: test
 -- ------------------------------------------------------
 -- Server version	5.7.20-log
 
@@ -14,3 +14,134 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `exogen_user`
+--
+
+DROP TABLE IF EXISTS `exogen_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exogen_user` (
+  `user_id` bigint(20) NOT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `name_1` varchar(255) DEFAULT NULL,
+  `name_2` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone_1` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `dob` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `uniquelogin` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exogen_user`
+--
+
+LOCK TABLES `exogen_user` WRITE;
+/*!40000 ALTER TABLE `exogen_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exogen_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login` (
+  `user_id` int(11) DEFAULT NULL,
+  `token` varchar(40) DEFAULT NULL,
+  `login_time` datetime DEFAULT NULL,
+  `valid` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (10,'3W/rVZVIpzO9c0QbU6H31EmHJxo=','2017-11-17 03:21:10','');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `policy`
+--
+
+DROP TABLE IF EXISTS `policy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `policy` (
+  `policy_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `amount_paid` float(7,2) DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `policy_valid` bit(1) DEFAULT NULL,
+  `policy_details` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`policy_no`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `policy_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `policy`
+--
+
+LOCK TABLES `policy` WRITE;
+/*!40000 ALTER TABLE `policy` DISABLE KEYS */;
+INSERT INTO `policy` VALUES (2,1,'Health Insurance Policy 1',10000.00,'2017-11-22 00:00:00','\0','Policy Details 1'),(8,1,'Health Insurance Policy 2',20000.00,'2017-11-22 00:00:00','','Policy Details 2'),(9,1,'Health Insurance Policy 3',30000.00,'2016-11-22 00:00:00','','Policy Details 3'),(10,1,'Health Insurance Policy 4',40000.00,'2017-11-22 00:00:00','','Policy Details 4'),(11,1,'Health Insurance Policy 5',50000.00,'2016-11-22 00:00:00','','Policy Details 5');
+/*!40000 ALTER TABLE `policy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(100) DEFAULT NULL,
+  `name_1` varchar(255) DEFAULT NULL,
+  `name_2` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone_1` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `type` int(2) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'12120310','1212','1212','sad','121212121','admin@gmail.com','12121212','2017-11-01',0),(10,'asds0510','asds','asd','asdsadsad','12121212121','sdfsd@nn.com','12345678','2017-11-10',0),(11,'test0510','test_name_1','test_name_2','test_address','+9102202020','test@test.com','testpassword','2017-11-17',0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-17 11:43:03
